fc1dfbb9adadd09b952d7ca7bca572472c3e1177
UlisesRosales2004 <ulises223366@gmail.com>
2024-08-27 13:44:45 -0300

    Adaptaciones para el Front

--- src/main/java/com/rosales/APIBazar/controller/ClienteController.java
+++ src/main/java/com/rosales/APIBazar/controller/ClienteController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "")
+@CrossOrigin(origins = "*")
 @RestController
 public class ClienteController {
     @Autowired
--- src/main/java/com/rosales/APIBazar/controller/VentaController.java
+++ src/main/java/com/rosales/APIBazar/controller/VentaController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-@CrossOrigin(origins = "")
+@CrossOrigin(origins = "*")
 @RestController
 public class VentaController {
     @Autowired
