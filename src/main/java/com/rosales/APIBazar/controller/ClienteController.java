d7009ead4ba7bd1a5505acd349df043df8d85a7b
UlisesRosales2004 <ulises223366@gmail.com>
2024-08-27 13:43:47 -0300

    Adaptaciones para el Front

--- src/main/java/com/rosales/APIBazar/controller/ClienteController.java
+++ src/main/java/com/rosales/APIBazar/controller/ClienteController.java
@@ -5,6 +5,7 @@ import com.rosales.APIBazar.model.Cliente;
 import com.rosales.APIBazar.service.IClienteService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "")
 @RestController
 public class ClienteController {
     @Autowired
--- src/main/java/com/rosales/APIBazar/controller/ProductoController.java
+++ src/main/java/com/rosales/APIBazar/controller/ProductoController.java
@@ -5,6 +5,7 @@ import com.rosales.APIBazar.model.Producto;
 import com.rosales.APIBazar.service.IProductoService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+@CrossOrigin(origins = "")
 @RestController
 public class ProductoController {
     @Autowired
