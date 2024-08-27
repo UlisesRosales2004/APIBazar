d11092bd27a1031af202d99b6ff12a8d1fe36233
UlisesRosales2004 <ulises223366@gmail.com>
2024-08-27 14:12:52 -0300

    Adaptaciones para el Front

--- src/main/java/com/rosales/APIBazar/controller/ClienteController.java
+++ src/main/java/com/rosales/APIBazar/controller/ClienteController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*")
@@ -20,22 +21,27 @@ public class ClienteController {
     @Autowired
     IClienteService cliServ;
     @PostMapping("/clientes/crear")
+    @ResponseBody
     public String saveCliente(@RequestBody Cliente clie){
         return cliServ.SaveCliente(clie);
     }
     @GetMapping("/clientes")
+    @ResponseBody
     public List<Cliente> getClientes(){
         return cliServ.GetClientes();
     }
     @GetMapping("/clientes/{id_cliente}")
+    @ResponseBody
     public Cliente findCliente(@PathVariable Long id_cliente){
         return cliServ.FindCliente(id_cliente);
     }
     @DeleteMapping("/clientes/eliminar/{id_cliente}")
+    @ResponseBody
     public String deleteCliente(@PathVariable Long id_cliente){
         return cliServ.DeleteCliente(id_cliente);
     }
     @PutMapping("/clientes/editar/{id_cliente}")
+    @ResponseBody
     public String editCliente(@PathVariable Long id_cliente,@RequestBody Cliente clie){
         return cliServ.EditCliente(id_cliente, clie);
     }
